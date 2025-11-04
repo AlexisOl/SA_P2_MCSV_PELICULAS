bdc7a545f5f36f0e0804276fa0aa5da6932513c6
Alexxus <85314303+AlexisOl@users.noreply.github.com>
2025-11-04 01:13:51 -0600

    prueba de quitar el cors momentaneamente

--- src/main/java/com/movies/microservice/common/config/WebConfig.java
+++ src/main/java/com/movies/microservice/common/config/WebConfig.java
@@ -5,26 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+// @Configuration
 public class WebConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todos los endpoints
-                        .allowedOrigins(
-                            "http://localhost:4200",       // Angular local
-                            "http://127.0.0.1:4200",
-                            "https://frontend-peliculas.vercel.app" // si lo despliegas
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .exposedHeaders("Authorization", "Content-Disposition")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
-    }
+    // @Bean
+    // public WebMvcConfigurer corsConfigurer() {
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //             registry.addMapping("/**") // Aplica a todos los endpoints
+    //                     .allowedOrigins(
+    //                         "http://localhost:4200",       // Angular local
+    //                         "http://127.0.0.1:4200",
+    //                         "https://frontend-peliculas.vercel.app" // si lo despliegas
+    //                     )
+    //                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+    //                     .allowedHeaders("*")
+    //                     .exposedHeaders("Authorization", "Content-Disposition")
+    //                     .allowCredentials(true)
+    //                     .maxAge(3600);
+    //         }
+    //     };
+    // }
 }
